05b57b51b97a13795755aec8c673fa2182b16b53
Arthurleriche <lerichearthur@gmail.com>
2020-07-06 15:36:29 +0200

    done

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -4,11 +4,3 @@ const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
 
-import AntdScssThemePlugin from 'antd-scss-theme-plugin';
-
-const webpackConfig =  {
-  // ...
-  plugins: [
-    new AntdScssThemePlugin('./theme.scss'),
-  ],
-};
